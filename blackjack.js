4ec4cbb2f42ec5921fbf4cd8776c3d1de2ff6951
Stefan Salcianu <stefansalcianu26@gmail.com>
2025-02-10 20:07:41 +0200

    change cards style, implemented dealerHit idea, implementing changing cards, fixed bugs

--- blackjack.js
+++ blackjack.js
@@ -14,12 +14,16 @@ document.addEventListener("DOMContentLoaded", () => {
   const hit4 = document.getElementById("player_card4");
   const hit5 = document.getElementById("player_card5");
   const hit6 = document.getElementById("player_card6");
-
+  const dealer3 = document.getElementById("dealer_card3");
+  const dealer4 = document.getElementById("dealer_card4");
+  const dealer5 = document.getElementById("dealer_card5");
+  const dealer6 = document.getElementById("dealer_card6");
   let hidden_card;
   let hidden_card_value;
   let value_player_cnt = 0;
   let value_dealer_cnt = 0;
   let cards_player = 0;
+  let cards_dealer = 2;
 
   if (!localStorage.getItem("loses")) {
     localStorage.setItem("loses", "0");
@@ -66,6 +70,7 @@ document.addEventListener("DOMContentLoaded", () => {
   hit.disabled = true;
   stay.disabled = true;
   again.disabled = true;
+  let blackjack = false;
 
   fetch("blackjack.json")
     .then((response) => response.json())
@@ -78,8 +83,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   ///deal event
 
-  deal.addEventListener("click", () => {
-    console.log("deal");
+  function deal_cards() {
     randomi = Math.floor(Math.random() * cards.length);
     dealer1.src = cards[randomi].image;
     if (cards[randomi].value != "A") {
@@ -108,7 +112,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     value_player.innerText = "Your value: " + String(value_player_cnt);
     cards.splice(randomj, 1);
-    dealer2.src = "images/oie_jpg.png";
+    dealer2.src = "images/back_card.png";
     randomi = Math.floor(Math.random() * cards.length);
     hidden_card = cards[randomi].image;
     if (cards[randomi].value == "A") {
@@ -120,6 +124,21 @@ document.addEventListener("DOMContentLoaded", () => {
     deal.disabled = true;
     hit.disabled = false;
     stay.disabled = false;
+    if (value_player_cnt == 21) {
+      console.log("sex");
+      blackjack = true;
+      setTimeout(() => {
+        turnHiddenCard();
+        ///implement BLACKJACK animation
+        setTimeout(() => {
+          play_again();
+        }, 3000);
+      }, 700);
+    }
+  }
+  deal.addEventListener("click", () => {
+    console.log("deal");
+    deal_cards();
   });
   hit.addEventListener("click", () => {
     ///pica cazul cand primseti 2 asi
@@ -154,10 +173,10 @@ document.addEventListener("DOMContentLoaded", () => {
       } else {
         value_player_cnt += parseInt(cards[randomj].value11);
       }
+      cards.splice(randomj, 1);
       if (value_player_cnt > 21) {
         busted = true;
         const busted_text = document.createElement("p");
-        busted_text.classList.add("busted");
         busted_text.innerText = "YOU BUSTED";
         busted_text.style.fontFamily = "'Times New Roman', Times, serif";
         busted_text.style.fontSize = 40 + "px";
@@ -165,7 +184,7 @@ document.addEventListener("DOMContentLoaded", () => {
         busted_text.style.left = "50%";
         busted_text.style.top = "55%";
         busted_text.style.transform = "translate(-50%,-50%)";
-        busted_text.style.color = "red";
+        busted_text.style.color = "#8A0303";
         loses = parseInt(localStorage.getItem("loses"));
         loses += 1;
         loses_text.innerText = "Loses: " + String(loses);
@@ -176,51 +195,111 @@ document.addEventListener("DOMContentLoaded", () => {
         stay.disabled = true;
         hit.disabled = true;
         setTimeout(() => {
+          if (busted == true) table.removeChild(busted_text);
           play_again();
         }, 2000);
       } else if (value_player_cnt == 21) {
         ///incepe dealerHit
         stay.disabled = true;
+        hit.disabled = true;
+        setTimeout(() => {
+          if (value_dealer_cnt < 17) {
+            dealer2.style.left = "39%";
+            dealer1.style.left = "28%";
+          }
+          setTimeout(() => {
+            dealerHit(cards_dealer);
+            setTimeout(() => {
+              play_again();
+            }, 2000);
+          }, 700);
+        }, 700);
+        turnHiddenCard();
       }
       value_player.innerText = "Your value: " + String(value_player_cnt);
     } else {
       if (value_player_cnt == 21) {
         ///incepe dealerHit
         stay.disabled = true;
+        hit.disabled = true;
+        setTimeout(() => {
+          setTimeout(() => {
+            dealerHit(cards_dealer);
+            setTimeout(() => {
+              play_again();
+            }, 2000);
+          }, 700);
+        }, 700);
+        turnHiddenCard();
       }
     }
   });
   stay.addEventListener("click", () => {
+    stay.disabled = true;
     setTimeout(() => {
-      dealer2.style.left = "39%";
-      dealer1.style.left = "28%";
+      if (value_dealer_cnt < 17) {
+        dealer2.style.left = "39%";
+        dealer1.style.left = "28%";
+      }
+      setTimeout(() => {
+        dealerHit(cards_dealer);
+        setTimeout(() => {
+          play_again();
+        }, 2000);
+      }, 700);
     }, 700);
+    hit.disabled = true;
     turnHiddenCard();
   });
   function play_again() {
-    value_player_cnt = 0;
-    value_dealer_cnt = 0;
-    cards_player = 0;
-    busted = false;
-    game_over = false;
-    hit.disabled = true;
-    stay.disabled = true;
-    deal.disabled = false;
-    mockText.innerText = "Still here?:)";
-    dealer1.src = "";
-    dealer2.src = "";
-    player.src = "";
-    player0.src = "";
-    hit1.src = "";
-    hit2.src = "";
-    hit3.src = "";
-    hit4.src = "";
-    hit5.src = "";
-    hit6.src = "";
-    value_dealer.innerText = "Dealer value: " + String(value_dealer_cnt);
-    value_player.innerText = "Your value: " + String(value_dealer_cnt);
-    busted_text = document.querySelector(".busted");
-    table.removeChild(busted_text);
+    if (cards.length < 15) {
+      setTimeout(() => {
+        table.removeChild(change_text);
+        location.reload();
+      }, 3000); // Reloads after 5 seconds
+      const change_text = document.createElement("p");
+      change_text.innerText = "Changing deck...";
+      change_text.style.fontFamily = "'Times New Roman', Times, serif";
+      change_text.style.fontSize = 40 + "px";
+      change_text.style.position = "absolute";
+      change_text.style.left = "50%";
+      change_text.style.top = "55%";
+      change_text.style.transform = "translate(-50%,-50%)";
+      change_text.style.color = "#8A0303";
+      table.appendChild(change_text);
+      hit.disabled = true;
+      stay.disabled = true;
+    } else {
+      value_player_cnt = 0;
+      value_dealer_cnt = 0;
+      cards_player = 0;
+      game_over = false;
+      blackjack = false;
+      hit.disabled = true;
+      stay.disabled = true;
+      mockText.innerText = "Still here?:)";
+      dealer1.src = "";
+      dealer2.src = "";
+      dealer3.src = "";
+      dealer4.src = "";
+      dealer5.src = "";
+      dealer6.src = "";
+      player.src = "";
+      player0.src = "";
+      hit1.src = "";
+      hit2.src = "";
+      hit3.src = "";
+      hit4.src = "";
+      hit5.src = "";
+      hit6.src = "";
+      value_dealer.innerText = "Dealer value: " + String(value_dealer_cnt);
+      value_player.innerText = "Your value: " + String(value_dealer_cnt);
+      busted_text = document.querySelector(".busted");
+      busted = false;
+      dealer1.style.left = "44%";
+      dealer2.style.left = "56%";
+      deal_cards();
+    }
   }
 
   function turnHiddenCard() {
@@ -238,7 +317,40 @@ document.addEventListener("DOMContentLoaded", () => {
     value_dealer_cnt += hidden_card_value;
     value_dealer.innerText = "Dealer value: " + String(value_dealer_cnt);
   }
-  function dealerHit() {
+  function dealerHit(cards_dealer) {
+    if (value_dealer_cnt >= 17) {
+      // checkWinner();
+      return;
+    }
+    cards_dealer += 1;
+    randomi = Math.floor(Math.random() * cards.length);
+    switch (cards_dealer) {
+      case 3:
+        dealer3.src = cards[randomi].image;
+        break;
+      case 4:
+        dealer4.src = cards[randomi].image;
+        break;
+      case 5:
+        dealer5.src = cards[randomi].image;
+        break;
+      case 6:
+        dealer6.src = cards[randomi].image;
+        break;
+      default:
+        break;
+    }
+    if (cards[randomi].value != "A") {
+      value_dealer_cnt += parseInt(cards[randomi].value);
+    } else {
+      value_dealer_cnt += parseInt(cards[randomi].value11);
+    }
+    cards.splice(randomi, 1);
+    value_dealer.innerText = "Dealer value: " + String(value_dealer_cnt);
+    setTimeout(() => {
+      dealerHit(cards_dealer);
+    }, 700);
+
     ///to be implemented
     ///vezi switch de la hit
   }
